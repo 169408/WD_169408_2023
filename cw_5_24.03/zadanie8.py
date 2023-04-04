a1d025f850a3da569ef66419d070e56f339f5e84
Ruslan <115550042+169408@users.noreply.github.com>
2023-04-04 21:17:47 +0200

    Add files via upload

---
+++ cw_5_24.03/zadanie8.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+mat = np.arange(1, 10).reshape(3, 3)
+
+for row in mat:
+    print(row)
---
+++ cw_5_24.03/zadanie9.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+tab = np.array([[2, 4, 8], [1, 3, 5], [10, 9, 2]])
+
+for i in tab.flatten():
+    print(i)
